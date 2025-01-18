5af1a9cd0e7008b51b7bcd25dc4750e7aef4a4c3
NikP295 <nikposedi295@gmail.com>
2025-01-18 19:36:04 +0000

    First functional version (program runs) Grid is made, coords are assigned

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,65 @@
+use eframe::egui;
+//use std::io;
+
+
 fn main() {
     //println!("Runs");
-    use eframe::egui;
-    use std::io;
     //io::stdin().readline(&mut usr_input).expect("Failed to read the user input!");
     let window_options = eframe::NativeOptions::default();
+    let x = 8;
     eframe::run_native(
         "Nikov Minesweeper",
         window_options,
-        box::new(|_cc| Ok(box::new(sweeper_of_mines::size(x)))),
+        Box::new(|_cc| Ok(Box::new(SweeperOfMines::size(x)))),
     );
 }
 
-struct sweeper_of_mines {
-    grid_size:i32,
-    bomb_num:i32;
+// struct SweeperOfMines {
+//     grid_size:i32,
+//     bomb_num:i32;
+// }
+struct SweeperOfMines{
+    bomb_num:i32,
+    grid_size:i32
 }
 
-impl size for sweeper_of_mines {
-    fn size(x: i32) -> self {
-        grid_size = x;
-        bomb_num = match x {
-            8 -> 10,
-            16 -> 40,
-            32 -> 99,
-            _ -> 10
+trait Size {
+    fn size(x:i32) -> Self;
+}
+
+impl Size for SweeperOfMines {
+    fn size(x: i32) -> Self {
+        let bomb_num = match x {
+            8 => 10,
+            16 => 40,
+            32 => 99,
+            _ => 10
+        };
+        
+        SweeperOfMines {
+            grid_size: x,
+            bomb_num
         }
     }
 }
 
-impl eframe::app for sweeper_of_mines {
+impl eframe::App for SweeperOfMines {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             let size_display:i32 = self.grid_size;
-            ui.heading("Minesweeper: {}x{}", size_display, size_display);
-            // left off here
+            ui.heading(format!("Minesweeper: {}x{}", size_display, size_display));
+            ui.vertical(|ui| {
+                for row in 0..size_display {
+                    ui.horizontal(|ui| {
+                        for col in 0..size_display {
+                            if ui.button(format!("{}x{}", row, col)).clicked() {
+                                println!("Tile at {},{} clicked!", row, col);
+                            }
+                        }
+                    });
+                }
+            });
+            
         });
     }
 }
