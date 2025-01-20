36c2dcff996c6eac9ca65ceca1f042d9866de1c3
NikP295 <nikposedi295@gmail.com>
2025-01-20 17:45:38 +0000

    Added: uncovering of the clicked square automatic uncovering of squares adjacent to the clicked uncovered square flag logic started adding right click handling for setting flags
    NOT A FUNCTIONAL BUILD, BACKUP ONLY

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,14 @@ fn main() {
     );
 }
 
-// struct SweeperOfMines {
-//     grid_size:i32,
-//     bomb_num:i32;
-// }
+
 struct SweeperOfMines{
     bomb_num:i32,
     grid_size:i32,
     bomb_locations:Vec<(usize, usize)>,
-    grid:Vec<Vec<i32>>
+    grid:Vec<Vec<i32>>,
+    display_grid:Vec<Vec<bool>>,
+    playing_status:bool
 }
 
 trait Begin {
@@ -43,35 +42,90 @@ impl Begin for SweeperOfMines {
         };
         let mut grid = vec![vec![0; size as usize]; size as usize];
 
+        let mut display_grid = vec![vec![false; size as usize]; size as usize];
+
         let bomb_locations = bomb_locations(bomb_num, size);
 
         SweeperOfMines {
             grid_size: size,
             bomb_num,
             grid,
-            bomb_locations
+            bomb_locations,
+            display_grid,
+            playing_status: true
         }
     }
 }
 
 impl SweeperOfMines {
-    fn adjacent_changes(&mut self, coords:(usize, usize), click_or_set:bool) {
+    fn what_to_display(&mut self, coords:(usize, usize)) -> char {
+        if self.display_grid[coords.0][coords.1] {
+            let value_on_square:i32 = self.grid[coords.0][coords.1];
+            if value_on_square <= 8 {
+                return (value_on_square as u8 + b'0') as char;
+            }
+            if value_on_square >= 20 {
+                return 'F';
+            }
+            if value_on_square == 10 {
+                return 'B';
+            }
+            else {
+                return '?';
+            }
+        }
+        else {
+            return ' ';
+        }
+    }
+}
+
+// OK SOOO! NOTE! i can add a number to a bomb square to mark it as flagged, and then remove the number if the player unflags the square... easy dub fr
+
+impl SweeperOfMines {
+    fn adjacent_changes(&mut self, coords:(usize, usize), user_click:bool) {
         let mut mins_and_maxes:Vec<usize>;
         let mut r_min:usize;
         let mut c_min:usize;
         let mut r_max:usize;
         let mut c_max:usize;
+        let grid_size:usize = (self.grid_size - 1) as usize; 
 
         // user click handling
-        if click_or_set {
+        if user_click {
+            if !self.display_grid[coords.0][coords.1] {
+                self.display_grid[coords.0][coords.1] = true;
+                if self.grid[coords.0][coords.1] == 10 {
+                    self.playing_status = false;
+                }
+            }
+            else {
+                mins_and_maxes = space_around_coord(coords.0, coords.1, grid_size);
+                r_min = mins_and_maxes[0];
+                c_min = mins_and_maxes[1];
+                r_max = mins_and_maxes[2];
+                c_max = mins_and_maxes[3];
+    
+                for x in r_min..=r_max {
+                    for y in c_min..=c_max {
 
+                        if self.grid[x][y] <= 10 {
+                            self.display_grid[x][y] = true;
+
+                            if self.grid[x][y] == 10 {
+                                self.playing_status = false;
+                            }
+                        }
+                    }
+                }
+            }
         }
         // setting bombs
         else {
             for bomb_in_locations in &self.bomb_locations {
                 let row = bomb_in_locations.0;
                 let col = bomb_in_locations.1;
-                mins_and_maxes = space_around_coord(row, col, (self.grid_size-1) as usize);
+                mins_and_maxes = space_around_coord(row, col, grid_size);
                 r_min = mins_and_maxes[0];
                 c_min = mins_and_maxes[1];
                 r_max = mins_and_maxes[2];
@@ -92,12 +146,10 @@ impl SweeperOfMines {
                 }
             }
         }
-
     }
 }
 
 fn space_around_coord(row:usize, col:usize, end_num:usize) -> Vec<usize> {
-
     let mut row_min:usize;
     let mut col_min:usize;
     let mut row_max:usize;
@@ -159,14 +211,18 @@ impl eframe::App for SweeperOfMines {
                     ui.horizontal(|ui| {
 
                         for col in 0..size_display {
-                            if ui.button(format!("{}", self.grid[row as usize][col as usize])).clicked() {
-                                println!("Tile at {},{} clicked!", row, col);
+                            let button = ui.button(format!("            \n    {}    \n            ", self.what_to_display((row as usize, col as usize))));
+                            if button.clicked() {
+                                self.adjacent_changes((row as usize, col as usize), true);
+                                //println!("Tile at {},{} clicked!", row, col);
+                            }
+                            else if button.interact(egui::Sense::click()) {
+                                println!("Right click on {},{}", row, col);
                             }
                         }
                     });
                 }
             });
-            
         });
     }
 }
